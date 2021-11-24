91cf7cccc204f4705a1a734bcfcde6f00edaba43
r97221004 <r97221004@ntu.edu.tw>
2021-11-24 15:36:08 +0800

    準備部屬

--- Hotel/config.py
+++ Hotel/config.py
@@ -4,16 +4,16 @@ import os
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "flask123"
+    SECRET_KEY = os.getenv("SECRET_KEY", "flask123")
     JWT_EXPIRATION_DELTA = timedelta(seconds = 300)
     JWT_AUTH_URL_RULE = "/auth/login"
-    JWT_AUTH_HEADER_PREFIX = "FLASK"
+    JWT_AUTH_HEADER_PREFIX = os.getenv("JWT_AUTH_HEADER_PREFIX", "FLASK")
 
 class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", 'mysql+pymysql://root:Aa137540@localhost:3306/demo')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", f'mysql+pymysql://root:{os.getenv("PASSWORD")}@localhost:3306/demo')
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", 'sqlite:///data.db')
